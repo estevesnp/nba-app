910df1d7820e9b4665de60e04bbd796d7ec29dc1
esteves <nuno.pinto@live.com>
2024-03-27 15:41:39 +0000

    added simple http server

---
+++ backend/go-api/go.mod
@@ -0,0 +1,3 @@
+module esteves/nba-api-server
+
+go 1.22.1
---
+++ backend/go-api/server.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+const port string = ":8080"
+
+func main() {
+
+	router := http.NewServeMux()
+	router.HandleFunc("/hello", handleHello)
+
+	server := http.Server{
+		Addr:    port,
+		Handler: router,
+	}
+
+	log.Printf("Starting server on port %s\n", port)
+	err := server.ListenAndServe()
+
+	if err != nil {
+		log.Println(err)
+	}
+
+	log.Println("Shutting down server...")
+}
+
+func handleHello(w http.ResponseWriter, r *http.Request) {
+	log.Printf("/hello request from %s\n", r.RemoteAddr)
+	w.Write([]byte("Hello World"))
+}
