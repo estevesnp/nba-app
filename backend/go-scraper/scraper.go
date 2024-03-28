f62179fb8f2c6d723b08ab176fb0eaded37d7239
esteves <nuno.pinto@live.com>
2024-03-28 19:27:08 +0000

    finished json parsing and created test jsons

--- backend/go-scraper/scraper.go
+++ backend/go-scraper/scraper.go
@@ -1,64 +1,150 @@
 package main
 
 import (
+	"bytes"
 	"compress/gzip"
-	"io"
+	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
 )
 
-func main() {
-	downloadPlayerJson()
+type Player struct {
+	Id       int    `json:"id"`
+	Name     string `json:"name"`
+	Position string `json:"position"`
+	Team     string `json:"team"`
 }
 
-func downloadPlayerJson() {
-	client := &http.Client{}
+func main() {
+	data, err := downloadPlayerData()
+	if err != nil {
+		log.Fatal("Error downloading player data:", err)
+	}
+
+	players, err := parsePlayerData(data)
+	if err != nil {
+		log.Fatal("Error parsing player data:", err)
+	}
+
+	err = savePlayersToJSON(players)
+	if err != nil {
+		log.Fatal("Error saving player data:", err)
+	}
+
+}
+
+func downloadPlayerData() ([]byte, error) {
 	reqUrl := "https://stats.nba.com/stats/playerindex?College=&Country=&DraftPick=&DraftRound=&DraftYear=&Height=&Historical=1&LeagueID=00&Season=2023-24&SeasonType=Regular%20Season&TeamID=0&Weight="
 
 	req, err := http.NewRequest("GET", reqUrl, nil)
-
 	if err != nil {
 		log.Fatal("Error creating request:", err)
 	}
 
-	req.Header.Add("Accept", "*/*")
-	req.Header.Add("Accept-Encoding", "gzip, deflate, br")
-	req.Header.Add("Accept-Language", "en-US,en;q=0.8")
-	req.Header.Add("Connection", "keep-alive")
-	req.Header.Add("Origin", "https://www.nba.com")
-	req.Header.Add("Referer", "https://www.nba.com")
-	req.Header.Add("Sec-Ch-Ua", "\"Not_A Brand\";v=\"8\", \"Chromium\";v=\"120\", \"Brave\";v=\"120\"")
-	req.Header.Add("Sec-Ch-Ua-Mobile", "?0")
-	req.Header.Add("Sec-Ch-Ua-Platform", "\"Linux\"")
-	req.Header.Add("Sec-Fetch-Dest", "empty")
-	req.Header.Add("Sec-Fetch-Mode", "cors")
-	req.Header.Add("Sec-Fetch-Site", "same-site")
-	req.Header.Add("Sec-Gpc", "1")
-	req.Header.Add("User-Agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36")
+	headers := map[string]string{
+		"Accept":             "*/*",
+		"Accept-Encoding":    "gzip, deflate, br",
+		"Accept-Language":    "en-US,en;q=0.8",
+		"Connection":         "keep-alive",
+		"Origin":             "https://www.nba.com",
+		"Referer":            "https://www.nba.com",
+		"Sec-Ch-Ua":          "\"Not_A Brand\";v=\"8\", \"Chromium\";v=\"120\", \"Brave\";v=\"120\"",
+		"Sec-Ch-Ua-Mobile":   "?0",
+		"Sec-Ch-Ua-Platform": "\"Linux\"",
+		"Sec-Fetch-Dest":     "empty",
+		"Sec-Fetch-Mode":     "cors",
+		"Sec-Fetch-Site":     "same-site",
+		"Sec-Gpc":            "1",
+		"User-Agent":         "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+	}
+
+	for key, value := range headers {
+		req.Header.Add(key, value)
+	}
+
+	client := &http.Client{}
 
 	resp, err := client.Do(req)
 	if err != nil {
-		log.Fatal("Error sending request:", err)
+		return nil, err
 	}
 	defer resp.Body.Close()
 
 	gz, err := gzip.NewReader(resp.Body)
-
 	if err != nil {
-		log.Fatal("Error creating gzip reader:", err)
+		return nil, err
 	}
 	defer gz.Close()
 
-	file, err := os.Create("players.json")
+	buf := &bytes.Buffer{}
+	buf.ReadFrom(gz)
+	data := buf.Bytes()
 
+	return data, nil
+
+}
+
+func parsePlayerData(data []byte) ([]Player, error) {
+
+	currentPlayers := []Player{}
+
+	var m map[string]interface{}
+	err := json.Unmarshal(data, &m)
 	if err != nil {
-		log.Fatal("Error creating file:", err)
+		return nil, err
+	}
+
+	resultSets, ok := m["resultSets"].([]interface{})
+	if !ok {
+		return nil, fmt.Errorf("resultSets not found")
+	}
+
+	allPlayers, ok := resultSets[0].(map[string]interface{})["rowSet"].([]interface{})
+	if !ok {
+		return nil, fmt.Errorf("rowSet not found")
+	}
+
+	for _, player := range allPlayers {
+		p := player.([]interface{})
+
+		if p[25] != "2023" {
+			continue
+		}
+
+		currentPlayers = append(currentPlayers, Player{
+			Id:       int(p[0].(float64)),
+			Name:     fmt.Sprintf("%s %s", p[2], p[1]),
+			Position: p[11].(string),
+			Team:     p[9].(string),
+		})
+	}
+
+	if len(currentPlayers) == 0 {
+		return nil, fmt.Errorf("no players found")
+	}
+
+	return currentPlayers, nil
+}
+
+func savePlayersToJSON(players []Player) error {
+	data, err := json.Marshal(players)
+	if err != nil {
+		return err
+	}
+
+	file, err := os.Create("players.json")
+	if err != nil {
+		return err
 	}
 	defer file.Close()
 
-	_, err = io.Copy(file, gz)
+	_, err = file.Write(data)
 	if err != nil {
-		log.Fatal("Error copying data:", err)
+		return err
 	}
+
+	return nil
+
 }
