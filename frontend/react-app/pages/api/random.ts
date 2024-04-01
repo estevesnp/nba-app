73e7575055f0421a27a86b2a5c22f09599c37d7b
esteves <nuno.pinto@live.com>
2024-04-01 16:33:09 +0100

    configured frontend support for .env.local file

--- frontend/react-app/pages/api/random.ts
+++ frontend/react-app/pages/api/random.ts
@@ -7,9 +7,11 @@ export default async function handler(
 ) {
   const controller = new AbortController();
   const timeoutId = setTimeout(() => controller.abort(), 2000);
+  const host = process.env.BACKEND_HOST || "localhost";
+  const port = process.env.BACKEND_PORT || "9000";
 
   try {
-    const response = await fetch("http://backend:9000/random", {
+    const response = await fetch(`http://${host}:${port}/random`, {
       signal: controller.signal,
     });
     if (!response.ok) {
